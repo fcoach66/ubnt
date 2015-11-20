3922fd7359f508c94c1cf38877526baf3256f433
fcoach66 <fabrizio.cocciari@hotmail.it>
2015-11-20 07:00:40 +0100

    Initial commit

---
+++ unifi/unifi-controller-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "" >> /etc/apt/sources.list
+echo "deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti" >> /etc/apt/sources.list
+echo "" >> /etc/apt/sources.list
+echo "deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen" /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50 -y
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 -y
+apt-get update
+apt-get install -y unifi
+
